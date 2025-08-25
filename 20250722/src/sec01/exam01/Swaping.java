8f8b16b8b151d29af44ed84ca4dd4b63b008a862
kdh2212 <jjun8248@gmail.com>
2025-08-25 12:52:17 +0900

    Example Code

---
+++ 20250722/src/sec01/exam01/Swaping.java
@@ -0,0 +1,20 @@
+package sec01.exam01;
+
+public class Swaping {
+
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		int z = 0;
+		System.out.println("x:" +x +" y:" +y);
+		
+		
+		 z = y;
+		 y = x;
+		 x = z;
+		System.out.println("x:" + x + " y:" + y);
+
+
+	}
+
+}
---
+++ 20250722/src/sec01/exam01/gugudan_ex1.java
@@ -0,0 +1,18 @@
+package sec01.exam01;
+
+public class gugudan_ex1 {
+	public static void main(String[] args) {
+		for(int i=2; i<=9; i++) {
+		 for(int j= 1; j<=9; j++) {
+			//System.out.printf("%d단\t", i);
+				System.out.printf("%d X %d = %d\t", i,j,i*j);
+		}
+		 if (i ==5) {
+			 break;
+		 }
+			System.out.println();
+		  
+		}
+	}
+
+}
