5905af22340a292a662eaf1879de8a2de0baeeb9
kdh2212 <jjun8248@gmail.com>
2025-08-25 13:19:44 +0900

    Session : session을 활용한 Servlet 화면 처리 예제 / session 예제

---
+++ SessionLogin/src/main/java/com/du/Controlloer.java
@@ -0,0 +1,91 @@
+package com.du;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+/**
+ * Servlet implementation class Controlloer
+ */
+@WebServlet("/")
+public class Controlloer extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controlloer() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String view = null;
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String com = uri.substring(conPath.length());
+		
+		HttpSession session = request.getSession(); // 세션이 없으면 새로만들어주고 세션이 있으면 가져와 준다. false를 주면 만들지 않는다.있는것만 가져온다.
+		if(com.equals("/") || com.equals("/main")) {
+			String userId = (String)session.getAttribute("userId");
+		    if(userId != null) {
+		    	request.setAttribute("userId", userId);
+		    	view = "loginOK.jsp";
+		    }else {
+		    	view = "redirect:login_main.jsp";
+		    }
+			
+			
+		}else if(com.equals("/login")) {
+			String id = request.getParameter("id");
+		    String pw = request.getParameter("pw");
+			
+		    if (id.equals("admin") && pw.equals("1234")) {
+		        session.setAttribute("userId",   id      );
+		        session.setAttribute("userName", "관리자");
+		        
+		        view = "loginOK.jsp";
+		    }else {
+		    	view = "redirect:loginFail.jsp";
+		    }
+		
+		}else if(com.equals("/logout")) {
+			session.invalidate();
+		    
+		    view = "redirect:login_main.jsp";
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		if (view.startsWith("redirect:")) {
+			response.sendRedirect(view.substring(9));
+		} else {
+			request.getRequestDispatcher(view).forward(request, response);
+		}
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ SessionLogin/src/main/webapp/loginOK.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<p>${sessionScope.userId }</p>
+<p>${sessionScope.userName }</p>
+<h2>${sessionScope.userName }님 로그인</h2>
+<form action="logout" method="post">
+
+	<%=(String)session.getAttribute("userId")%>님 로그인
+	
+	<input type="submit" value="로그아웃">
+</form>
+</body>
+</html>
