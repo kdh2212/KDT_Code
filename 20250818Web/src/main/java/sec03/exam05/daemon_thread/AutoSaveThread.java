9bca94d8baf07f2b1ad1f27b658f6470c6e13719
kdh2212 <jjun8248@gmail.com>
2025-08-25 13:09:43 +0900

    Thread 예제: 데몬 스레드 예제

---
+++ 20250818Web/src/main/java/sec03/exam05/daemon_thread/AutoSaveThread.java
@@ -0,0 +1,22 @@
+package sec03.exam05.daemon_thread;
+
+public class AutoSaveThread extends Thread{
+	
+	
+	public void save() {
+		System.out.println("작업 내용을 저장함.");
+	}
+	
+	@Override
+	public void run() {
+		while(true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				break;
+			}
+			save();
+		}
+	}
+
+}
